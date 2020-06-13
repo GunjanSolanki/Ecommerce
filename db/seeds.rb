58f3ddf90bf9d9c0ed31f057c82dc214b8e38256
gsolanki <gsolanki@techracers.io>
2020-06-13 22:07:33 +0530

    add seed data & fix UI

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+User.create(name: "admin", email: "admin@onlineshop.com", 
+            contact: "7748020946", address: "100A/C silicon valley",
+            gender: "male", is_admin: true, password: "123123")
+
+
+User.create(name: "testuser", email: "testuser@onlineshop.com", 
+  contact: "7748020946", address: "101A/C silicon valley",
+  gender: "male", is_admin: false, password: "123123")
+
+Category.create(name: "book")
+Category.create(name: "electronics")
+Category.create(name: "utensils")
+Category.create(name: "groceries")
+Category.create(name: "clothes")
